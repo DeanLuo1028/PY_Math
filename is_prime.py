a70050cbff66825a4175ceda38dfd89fab60ca2f
DeanLuo <phjh1090226@gmail.com>
2025-05-02 23:08:07 +0800

    add is_prime.py and .gitnore

---
+++ is_prime.py
@@ -0,0 +1,36 @@
+def is_prime(x):
+    #print("x :",x)
+    if x <= 1: return False
+    if x == 2: return True
+    # 只檢查比 n 的平方根小的質數
+    sqrt = int(x ** 0.5) + 1
+    nums = list(range(2,sqrt+1))
+    #print("nums:", end=" ")
+    #print(nums)
+    for n in range(2,sqrt+1): 
+        if not (n in nums): continue
+        # 從nums中取用n，但你在迴圈裡對nums進行操作會有問題的！
+        #print("n :",n)
+        if x % n == 0:
+            return False 
+        else: # n無法整除x
+            # 刪除 nums 中 n與n的倍數
+            # 因為既然n無法整除x，n的倍數也不可能整除x
+            # 把它們刪了可以減少計算量
+            #print(x,"%",n,"!=0")
+            #print("nums:", end=" ")
+            #print(nums)
+            #print("len(nums):", len(nums))
+            for i in range(n, sqrt+1, n):
+                #print("i :",i)
+                if i in nums: nums.remove(i)
+                #print("nums:", end=" ")
+                #print(nums)
+                #del nums[i-2] 不能這樣做！因為刪除之後列表會變短i就不在nums[i-2]上了！
+            
+    return True
+
+
+for i in range(1001):
+    if is_prime(i):
+        print(i,sep="", end=", ")
